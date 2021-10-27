d5961b51d39efb73b32ae804f08e3f7133dea50a
Itay93 <91763021+Itay93@users.noreply.github.com>
2021-10-27 05:12:03 +0300

    edit tab panel

--- src/components/TabPanel.js
+++ src/components/TabPanel.js
@@ -1,13 +1,7 @@
 const TabPanel = (props) => {
   const { children, value, index, ...other } = props;
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
+    <div role="tabpanel" hidden={value !== index} id={index} {...other}>
       {value === index && <div>{children}</div>}
     </div>
   );
